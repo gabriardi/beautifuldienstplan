5068e5f6439b73c38e26df12c6a560beab0a09f4
Gabriele Ardizzone <gabri.ardi@gmail.com>
2021-10-06 14:18:05 +0200

    edit manifest, add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,12 +24,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Beautiful Dienstplan`,
+        short_name: `Beautiful Dienstplan`,
         start_url: `/`,
-        background_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
