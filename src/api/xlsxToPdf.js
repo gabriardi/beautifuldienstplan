24429c8bb4a2d49563f72ffa1a872f22fecac13e
Gabriele Ardizzone <gabri.ardi@gmail.com>
2023-10-24 11:55:19 +0200

    use XMLHttpRequest

--- src/api/xlsxToPdf.js
+++ src/api/xlsxToPdf.js
@@ -1,5 +1,3 @@
-import axios from "axios"
-
 const xlsxToPdf = async formattedXlsx => {
   const urlToFile = async (url, filename, mimeType = "text/plain") => {
     const res = await fetch(url)
@@ -8,24 +6,45 @@ const xlsxToPdf = async formattedXlsx => {
   }
 
   const myFile = await urlToFile(formattedXlsx, "temp.xlsx")
+
   const data = new FormData()
   data.append("inputFile", myFile, "temp.xlsx")
 
-  const apiUrl = "https://api.cloudmersive.com"
-  const apiPath = "/convert/xls/to/pdf"
-  const headers = {
-    "Content-Type": "multipart/form-data",
-    Apikey: process.env.GATSBY_CLOUDMERSIVE_API_KEY,
-  }
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.withCredentials = false
 
-  const response = await axios.post(apiUrl + apiPath, data, {
-    headers,
-    responseType: "blob",
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          const responseType = this.getResponseHeader("content-type")
+          const responseArrayBuffer = this.response
+
+          if (responseType.startsWith("application/pdf")) {
+            // Assuming the response is a PDF file
+            const blob = new Blob([responseArrayBuffer], { type: responseType })
+            const pdfUrl = URL.createObjectURL(blob)
+            resolve(pdfUrl)
+          } else {
+            // Handle other binary responses here
+            // For example, you could save the binary data to a file
+            // or process it as needed.
+          }
+        } else {
+          reject(new Error(`XHR request failed with status ${this.status}`))
+        }
+      }
+    })
+
+    xhr.open("POST", "https://api.cloudmersive.com/convert/xlsx/to/pdf")
+
+    xhr.setRequestHeader("Apikey", process.env.GATSBY_CLOUDMERSIVE_API_KEY)
+
+    // Set the response type to "arraybuffer" to receive binary data
+    xhr.responseType = "arraybuffer"
+
+    xhr.send(data)
   })
-
-  const pdfUrl = URL.createObjectURL(response.data)
-
-  return pdfUrl
 }
 
 export default xlsxToPdf
